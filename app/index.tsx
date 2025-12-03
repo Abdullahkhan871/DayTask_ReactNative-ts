0050a32405697ef0aa0dcf62e9614b39b90c2054
Abdullahkhan871 <akhan283785@gmail.com>
2025-12-03 14:36:12 +0530

    chore: setup NativeWind and Tailwind CSS configuration
    - Added babel.config.js for Babel configuration with NativeWind support.
    - Created global.css to include Tailwind's base, components, and utilities.
    - Configured metro.config.js to integrate NativeWind with Expo.
    - Added nativewind-env.d.ts for TypeScript support with NativeWind.
    - Established tailwind.config.js with content paths and NativeWind preset.

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-100">
+      <Text>Abdullah</Text>
+    </View>
+  );
+}
